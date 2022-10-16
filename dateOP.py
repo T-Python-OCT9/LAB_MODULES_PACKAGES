d7f1ea9fb97d6908a7e085aca46bd1993facc307
Samaa <Samaanawaf@gmail.com>
2022-10-16 23:07:23 +0300

    The lab modules and packages is Solved !

---
+++ dateOP.py
@@ -0,0 +1,5 @@
+from datetime import date
+
+def DateToday ():
+    #A function that when called prints the current date
+    print(f"The date today is : ",{date.today()})
---
+++ main.py
@@ -0,0 +1,4 @@
+from dateOP import DateToday
+'''- import dateOP
+- call the function that print the current date'''
+DateToday()
